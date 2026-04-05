92516e5570275a526af1d0b3e5017f4f4749de3e
Barnabiii <86511805+Barnabiii@users.noreply.github.com>
2026-04-05 18:41:51 +0200

    air movement added

--- player/States/PlayerJumpState.gd
+++ player/States/PlayerJumpState.gd
@@ -1,7 +1,9 @@
 class_name PlayerJumpState extends State
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-var air_speed: float = 3
+var acceleration: float = 2.
+var speed: float = 4.
+var rotation_speed: float = 10.
 
 func get_next_state(_input: InputPackage) -> String:
 	if Puppet.is_on_floor():
@@ -16,5 +18,16 @@ func enter() -> void:
 	Animator.play("Jump_Start")
 
 
-func physics_update(_input: InputPackage, delta: float) -> void:
+func physics_update(input: InputPackage, delta: float) -> void:
 	Puppet.velocity.y -= gravity * delta
+	
+	var CamBasis: Basis = Puppet.get_node("CamPivot").global_transform.basis
+	var direction: Vector3 = (CamBasis.z * input.input_direction.y + CamBasis.x * input.input_direction.x).normalized()
+
+	Puppet.velocity.x = lerp(Puppet.velocity.x, direction.x * speed, acceleration * delta)
+	Puppet.velocity.z = lerp(Puppet.velocity.z, direction.z * speed, acceleration * delta)
+	
+	if not direction:
+		return
+	var target_rotation: float = atan2(direction.x,direction.z)
+	Puppet.get_node("Mesh").rotation.y = lerp_angle(Puppet.get_node("Mesh").rotation.y, target_rotation, rotation_speed * delta)
